afd157fc0206dfd8c4b938249ff484b22e47b13d
ArhamPawle <67964537+ArhamPawle@users.noreply.github.com>
2022-02-08 19:38:06 +0530

    Create Mutations.py

---
+++ Mutations.py
@@ -0,0 +1,12 @@
+def mutate_string(string, position, character):
+    list1=list(string) 
+    list1[position]=character
+    string1=''.join(list1)
+
+    return string1
+
+if __name__ == '__main__':
+    s = input()
+    i, c = input().split()
+    s_new = mutate_string(s, int(i), c)
+    print(s_new)
